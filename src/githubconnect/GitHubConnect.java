9734d5290389166d6b4af53791f3a51c31a70a63
Mthokwa Thandisiwe <Mthokwa Thandisiwe@LAPTOP-KQMFU2TH>
2024-08-08 13:58:38 +0200

    First Commit

---
+++ src/githubconnect/GitHubConnect.java
@@ -0,0 +1,15 @@
+
+package githubconnect;
+
+/**
+ *
+ * @author Mthokwa Thandisiwe
+ */
+public class GitHubConnect {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
